a3e3ff13d7447c8affcde5de5fbf3cf3b8540050
Dima34 <vavilov9823@gmail.com>
2022-08-21 12:06:42 +0300

    remove in section cell number repeating

--- Assets/Scripts/Section.cs
+++ Assets/Scripts/Section.cs
@@ -21,24 +21,24 @@ public class Section : MonoBehaviour
 
         // Find cell half size
         float cellHalfSize = cell.localScale.x / 2;
-
         // Find one side margin which includes margins and cell half (because when we spawn we specify the center coords. We need that to create cells on side and don`t calculate the half + center coords each time)
         float sideMargin = cellHalfSize + _sideMarginSize;
-
         // Calculate section width includes margin
         float includedSectionWidth = transform.localScale.x - sideMargin * 2;
-
         // Calctulate gaps between cells
         float cellGap = includedSectionWidth / (_cellAmount == 1 ? _cellAmount : (_cellAmount - 1));
-
         // Get section horizontal vector from center to left
         Vector3 localLeftVector = transform.localRotation * Vector3.left;
-
         // Get margin included left side coords
         Vector3 marginIncludedLeftPoint = transform.position + (localLeftVector * (includedSectionWidth / 2));
 
-        // Get correct cell number
-        int correctCellNum = getRandomCorrectCellNumber();
+        // Get correct cell posistion
+        int correctCellPosition = getRandomCorrectCellNumber();
+        // Calculate correct number value;
+        int correctNumber = startNumber + (increment * OrdinalNumber);
+
+        // Create exception array to prevent cell number repeating
+        List<int> createdNumberArray = new List<int>(){correctNumber};
 
         // Spawn cells
         for (int i = 0; i < _cellAmount; i++)
@@ -48,14 +48,19 @@ public class Section : MonoBehaviour
             Transform spawnedCell = Instantiate(cell, spawnPoint, transform.rotation);
             spawnedCell.SetParent(transform);
 
-            // Calculate correct number value;
-            int cellNum = startNumber + (increment * OrdinalNumber);
+            int cellNumber;
 
             // (n-1) till (n+cellAmount) except of correct number 
-            if (correctCellNum != i)
-                cellNum = getRandomExcept(cellNum - 1, cellNum + _cellAmount, cellNum);
+            if (correctCellPosition != i)
+            {
+                cellNumber = getRandomExcept(correctNumber - 1, correctNumber + _cellAmount, createdNumberArray);
+                createdNumberArray.Add(cellNumber);
+            }
+            else
+                cellNumber = correctNumber;
 
-            setUpCell(spawnedCell, cellNum);
+            setUpCell(spawnedCell, cellNumber);
+            createdNumberArray.Add(correctNumber);
         }
     }
 
@@ -71,13 +76,14 @@ public class Section : MonoBehaviour
         return Random.Range(0, _cellAmount);
     }
 
-    int getRandomExcept(int min, int max, int exceptNumber){
+    int getRandomExcept(int min, int max, List<int> exceptNumberList)
+    {
         int number;
 
         do
         {
             number = Random.Range(min, max);
-        } while (number == exceptNumber);
+        } while (exceptNumberList.Contains(number));
 
         return number;
     }
