e7dfeeaaebd2dd876920cf1da9a94b68d3a4864a
Dima34 <57358582+Dima34@users.noreply.github.com>
2022-08-28 22:42:56 +0300

    cell animation basic made

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,22 +1,44 @@
 using UnityEngine;
 using TMPro;
-using System.Collections.Generic;
 using System;
+using System.Collections;
 
 public class Cell : MonoBehaviour
 {
     [SerializeField] TMP_Text _numberText;
-    public Action<Vector2> OnMoveEvent;    
+    [SerializeField] float _animTime = 2f;
+    public Action<Vector2> OnMoveEvent;
+
     
-    // Commented for development
-    // [HideInInspector]
+    [HideInInspector]
     public int Number;
 
     private void Awake() {
+        Debug.Log("Awake");
         SetNumber();
+        PlayAnim();
     }
 
     public void SetNumber(){
         _numberText.text = Number.ToString();
+    }
+
+    public void PlayAnim(){
+        StartCoroutine("playAnim");
+    }
+
+    IEnumerator playAnim(){
+        Debug.Log("Coroutine started, animTime - " + _animTime);
+        float t = 0;
+        Vector3 startScale = transform.localScale;
+
+        while(t < _animTime){
+            Debug.Log("statement - " + (t <= _animTime).ToString());
+            t += Time.deltaTime;
+            Debug.Log("t - " + t);
+
+            transform.localScale = startScale * Utils.EaseOutBounce(t);
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,14 +1,39 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public static class Utils {
-    public static float EaseInCirc(float x) {
+public static class Utils
+{
+    public static float EaseInCirc(float x)
+    {
         return 1 - Mathf.Sqrt(1 - Mathf.Pow(x, 2));
     }
 
     public static float EaseInOut(float x)
     {
         return x < 0.5 ? 16 * x * x * x * x * x : 1 - Mathf.Pow(-2 * x + 2, 5) / 2;
+    }
+
+    public static float EaseOutBounce(float x)
+    {
+        float n1 = 7.5625f;
+        float d1 = 2.75f;
+
+        if (x < 1 / d1)
+        {
+            return n1 * x * x;
+        }
+        else if (x < 2 / d1)
+        {
+            return n1 * (x -= 1.5f / d1) * x + 0.75f;
+        }
+        else if (x < 2.5 / d1)
+        {
+            return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+        }
+        else
+        {
+            return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+        }
     }
 
     public static int GetRandomExceptNumList(int min, int max, List<int> exceptNumberList)
