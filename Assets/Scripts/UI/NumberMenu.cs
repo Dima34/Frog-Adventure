03922adc4aabe9109f1b6cb2e8f9fd78d1751471
Dima34 <57358582+Dima34@users.noreply.github.com>
2022-10-07 20:03:37 +0300

    levels added, levels page level sort added

--- Assets/Scripts/UI/NumberMenu.cs
+++ Assets/Scripts/UI/NumberMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 using System;
 
 public class NumberMenu : MonoBehaviour
@@ -19,15 +20,40 @@ public class NumberMenu : MonoBehaviour
     void createSectionCards()
     {
         allLevels = LevelUtils.GetLevels();
-        
         HashSet<int> levelsBySection = new HashSet<int>();
 
         for (int i = 0; i < allLevels.Count; i++)
         {
             levelsBySection.Add(LevelUtils.GetLevelInfoByName(allLevels[i].name)[0]);
         }
+
+        List<int> sortedLevels = levelsBySection.ToList();
+
+        while(true){
+            bool isChanged = false;
+
+            for (int i = 0; i < sortedLevels.Count - 1; i++)
+            {
+                int current = sortedLevels[i];
+                int next = sortedLevels[i + 1];
+
+                if(current > next){
+                    int temp = next;
+                    sortedLevels[i + 1] = current;
+                    sortedLevels[i] = temp;
+                    isChanged = true;
+                }
+            }
+
+            if(isChanged){
+                isChanged = false;
+            } else
+            {
+                break;
+            }
+        }
         
-        foreach (var nameNum in levelsBySection)
+        foreach (var nameNum in sortedLevels)
         {
             NumberButton numButton = Instantiate(_numberButtonPrefab, new Vector3(), new Quaternion(), _buttonContainer.transform);
             numButton.SectionNumber = nameNum;
